8024e69033709ac5d3c802de51b7f00395df014f
mariozski <mariozski@outlook.com>
2014-11-02 22:33:58 +0100

    fixed gitignore

---
+++ Nebula.Cache/NoCache.fs
@@ -0,0 +1,15 @@
+﻿namespace Nebula.Cache
+
+open Nebula
+open System
+open System.Xml.Linq
+
+type NoCache() =
+
+    interface ICache with
+        member x.Get key =
+            None
+
+        member x.Set key value expiration =
+            ()
+
